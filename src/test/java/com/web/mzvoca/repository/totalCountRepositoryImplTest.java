0d4008b2951b13fe2f3bcd0ebb2fff629d0593f6
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2023-08-28 11:33:27 +0900

    test totalCountRead 테스트 완료

--- src/test/java/com/web/mzvoca/repository/totalCountRepositoryImplTest.java
+++ src/test/java/com/web/mzvoca/repository/totalCountRepositoryImplTest.java
@@ -1,15 +1,46 @@
 package com.web.mzvoca.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
+import static com.web.mzvoca.repository.DBConst.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@Slf4j
 class totalCountRepositoryImplTest {
 
+    TotalCountRepository totalCountRepository;
 
+    @BeforeEach
+    void beforeEach() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+
+        totalCountRepository = new TotalCountRepositoryImpl(dataSource);
+    }
 
     @Test
+    @DisplayName("총 제출 횟수 읽기")
     void totalCountRead() {
+        //given
+        Integer result = null;
+
+        //when
+//        assertThatThrownBy(() -> totalCountRepository.totalCountRead())
+//                .isInstanceOf(NoSuchElementException.class);
+        result = totalCountRepository.totalCountRead();
+
+        //then
+        assertThat(result).isEqualTo(3);
+
     }
 }
