2d3e8628c170534347e06617e8bd7e059c428777
whxogus215 <whxogus215@naver.com>
2023-09-11 12:44:38 +0900

    test questionWrongCountRead 테스트 완료

---
+++ src/test/java/com/web/mzvoca/repository/QuestionRepositoryImplTest.java
@@ -0,0 +1,49 @@
+package com.web.mzvoca.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static com.web.mzvoca.repository.DBConst.*;
+import static org.assertj.core.api.Assertions.*;
+
+class QuestionRepositoryImplTest {
+
+    QuestionRepository questionRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+
+        questionRepository = new QuestionRepositoryImpl(dataSource);
+    }
+
+
+    @Test
+    @DisplayName("특정 문항의 오류 횟수 읽기")
+    void questionWrongCountRead() {
+        //given
+        int questionNumber = 1;
+        Integer result = null;
+
+        //when
+        result = questionRepository.questionWrongCountRead(questionNumber);
+
+        //then
+        assertThat(result).isEqualTo(3);
+//        assertThatThrownBy(() -> questionRepository.questionWrongCountRead(questionNumber + 3))
+//                .isInstanceOf(NoSuchElementException.class);
+
+    }
+
+    @Test
+    void questionWrongCountUpdate() {
+    }
+}
